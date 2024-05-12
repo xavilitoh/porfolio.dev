7d2ac5ee1937170a2509b40a736dc887ea64980d
Warner Xavier Berroa Ortega <ing.wxbo@gmail.com>
2024-05-11 21:00:38 -0400

    wxbo changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 
-import robotsTxt from "astro-robots-txt"
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
-})
+  site: 'https://dev.wxbolab.com/',
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
